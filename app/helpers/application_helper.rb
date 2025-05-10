862e5b1d6717e268ceb52158cf37545ccbd76d46
satoeri0502 <sato.eriko0502@gmail.com>
2025-05-10 15:33:05 +0900

    Fix: OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,31 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'エンジニアみくじ',
+      title: 'エンジニアみくじ',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'あなたの今日のエンジニア運を占います',
+      keywords: 'エンジニア,おみくじ,くじ,診断メーカー',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('icon.png') }
+      ],
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@satoeri_63b',
+        image: image_url('ogp.png')
+      }
+    }
+  end
 end
