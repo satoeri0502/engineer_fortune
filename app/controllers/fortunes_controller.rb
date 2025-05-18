725b5eb107733052d451ea5eb0676e1f557b65c2
satoeri0502 <sato.eriko0502@gmail.com>
2025-05-18 15:55:23 +0900

    Fix: DBを使わない形に修正

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,33 +1,38 @@
 class FortunesController < ApplicationController
+  FORTUNE_LIST = {
+    error_rate: [
+      "０％！カンペキの予感…！",
+      "低め！スムーズに進むね！",
+      "普通！落ち着いて調査しよう！",
+      "高め…！無理せず、息抜きも大事かも…",
+      "爆発寸前！？！？一旦PCの電源落としましょうか！？！？"
+    ],
+    bug_fortune: [
+      "大吉！バグが起きない素敵な１日！", 
+      "中吉！スラスラ書けるかも！",
+      "小吉！今までの知識を思い出せば乗り越えられる…！", 
+      "凶…！独り言をつぶやきながらバグと戦いましょう…", 
+      "バグ地獄！…気分転換したほうが良いかも？"
+    ],
+    commit_power: [
+      "爆上がり！！草超えて草原超えて森が生まれる…！！",  
+      "絶好調！どんどんcommitできる！",
+      "まあまあ！たくさん草を生やせそう！",
+      "微妙…。１つでもcommit出来たら上出来！",
+      "枯れ枯れ…。明日はcommitできるはず…！"
+    ]
+  }.freeze
+  
   def index
-    @fortune = nil
   end
 
   def create
-    @fortune = Fortune.create(
-      error_rate: [
-        "０％！カンペキの予感…！",
-        "低め！スムーズに進むね！",
-        "普通！落ち着いて調査しよう！",
-        "高め…！無理せず、息抜きも大事かも…",
-        "爆発寸前！？！？一旦PCの電源落としましょうか！？！？"
-      ].sample,
-      bug_fortune: [
-        "大吉！バグが起きない素敵な１日！", 
-        "中吉！スラスラ書けるかも！",
-        "小吉！今までの知識を思い出せば乗り越えられる…！", 
-        "凶…！独り言をつぶやきながらバグと戦いましょう…", 
-        "バグ地獄！…気分転換したほうが良いかも？"
-      ].sample,
-      commit_power: [
-        "爆上がり！！草超えて草原超えて森が生まれる…！！",  
-        "絶好調！どんどんcommitできる！",
-        "まあまあ！たくさん草を生やせそう！",
-        "微妙…。１つでもcommit出来たら上出来！",
-        "枯れ枯れ…。明日はcommitできるはず…！"
-      ].sample
-    )
-
-    render 'index'
+    @fortune = {
+      error_rate:   FORTUNE_LIST[:error_rate].sample,
+      bug_fortune:  FORTUNE_LIST[:bug_fortune].sample,
+      commit_power: FORTUNE_LIST[:commit_power].sample
+    }
+    logger.debug(@fortune.inspect)
+    render template: 'fortunes/index'
   end
 end
